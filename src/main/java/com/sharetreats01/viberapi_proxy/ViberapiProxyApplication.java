e41e78f0f3b11d3c94ff7341d2a07163c9b13f63
dlwlgh9412 <dlwlgh9412@gmail.com>
2023-06-19 15:35:32 +0900

    init

---
+++ src/main/java/com/sharetreats01/viberapi_proxy/ViberapiProxyApplication.java
@@ -0,0 +1,13 @@
+package com.sharetreats01.viberapi_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViberapiProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViberapiProxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sharetreats01/viberapi_proxy/ViberapiProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharetreats01.viberapi_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViberapiProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
